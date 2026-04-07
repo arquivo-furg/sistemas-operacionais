e69afb58e9796f661355a573ed106f7e2eead264
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2026-04-07 03:59:18 +0000

    feat: implement signal handling for SIGTERM and SIGINT

--- A1.c
+++ A1.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <signal.h>
 #include <unistd.h>
 
 #define X 20
 
+static void tratador_sinal(int s)
+{
+  printf("\nsinal recebido <%d>\n", s);
+}
+
 int main()
 {
+  struct sigaction sa;
+  sa.sa_handler = tratador_sinal;
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGTERM, &sa, NULL); // Trata o sinal SIGTERM <15>
+  sigaction(SIGINT, &sa, NULL); // Trata o sinal SIGINT <2>
+
   int i = 0, p = getpid();
   printf("PID <%d> diz: Ola\n", p);
 
